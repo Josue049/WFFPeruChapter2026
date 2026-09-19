04bd2ef7f0df403e6557b573b7d58f2cb9128b07
Josue049 <josue09guti@gmail.com>
2026-09-19 08:13:36 -0500

    fix close

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ function App() {
             </ProtectedRoute>
           }
         />
+        
         <Route path="/loginadmin" element={<LoginAdmin />} />
 
         <Route path="/Admin" element={<Navigate to="/admin" replace />} />
